e4ce6fbeda9b549dc754a32cae88d10b6cf88807
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2018-04-29 19:55:23 +0200

    added users controller register view

---
+++ app/controllers/Users.php
@@ -0,0 +1,39 @@
+<?php
+
+ class Users extends Controller {
+
+  public function __construct(){
+
+  }
+
+  public function register(){
+    //check for POST
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+      // Proccess the Form
+    } else {
+      // Init data
+      $data =[
+        'name' => '',
+        'email' => '',
+        'password' => '',
+        'confirm_password' => '',
+        'name_err' => '',
+        'email_err' => '',
+        'password_err' => '',
+        'confirm_password_err' => ''
+      ];
+
+      // Load view
+      $this->view('users/register', $data);
+    }
+
+  }
+
+  public function index(){
+
+  }
+
+ }
+
+
+?>
